bc510bf2fbfa2c1d876bee12dcf917a2e80c5a6e
dollbo <annadolly.dollbo@gmail.com>
2022-02-19 20:20:40 +0100

    Halfway there

--- Calibration2.cpp
+++ Calibration2.cpp
@@ -10,6 +10,9 @@
 #include <stdio.h>
 #include <iostream>
 #include<typeinfo>
+#include<iterator>
+#include <algorithm>
+#include<vector>
 
 //#define CV_CALIB_CB_ADAPTIVE_THRESH 1
 //#define CV_CALIB_CB_FAST_CHECK 8
@@ -65,7 +68,7 @@ int main() {
   fs << "overallRMS_RPError" << rmsRP_Error; 
   fs.release();
 
-  std::cout << "cameraMatrix : " << cameraMatrix << std::endl;
+  std::cout << "cameraMatrix : "<< cameraMatrix << std::endl;
   std::cout << "distCoeffs : " << distCoeffs << std::endl;
   std::cout << "Rotation vector : " << R << std::endl;
   std::cout << "Translation vector : " << T << std::endl;
@@ -84,6 +87,39 @@ void print(std::vector <int> const &a) {
    }
    std::cout << std::endl;
    
+}
+
+// int getIndex(std::vector<cv::String> v, int K)
+// {
+//     auto it = find(v.begin(), v.end(), K);
+ 
+//     // If element was found
+//     if (it != v.end())
+//     {
+     
+//         // calculating the index
+//         // of K
+//         int index = it - v.begin();
+//         return index;
+//     }
+// }
+
+
+/*Function to add zeros to index position of images to be ignored
+*This makes sure that the perViewErrorsVector is always equal to the amount of images used
+*which will make it possible to ignore the images that gives the highest perViewError even 
+*when the vector of perViewErrors is getting smaller
+
+*/
+void insertZeros(std::vector<float> vec){
+
+  for(int i{0}; i<imagesToIgnore.size(); i++){
+    
+      auto itPos = imagesToIgnore.begin() + imagesToIgnore[i];
+      float zero = 0.0;
+      vec.insert(itPos, zero);
+  }
+
 }
 
 double iterationBody() {
@@ -195,12 +231,24 @@ double iterationBody() {
   //CALIB_USE_INTRINSIC_GUESS
 
   //Transforming perViewErrors to vector, in order to get indices in next step
-  cv::Mat flat = perViewErrors.reshape(1, perViewErrors.total()*perViewErrors.channels());
-  std::vector<uchar> perViewErrorsVector = perViewErrors.isContinuous()? flat : flat.clone();
+  // cv::Mat flat = perViewErrors.reshape(1, perViewErrors.total()*perViewErrors.channels());
+  // std::vector<uchar> perViewErrorsVector = perViewErrors.isContinuous()? flat : flat.clone();
+
+  std::vector<float> perViewErrorsVector;
+  //cv::Mat res(m, n, CV_32FC1);
+  perViewErrorsVector.assign(perViewErrors.begin<float>(), perViewErrors.end<float>());
+  
+
+  //Passing vector to function "___" to make sure that index taken in next step correspond to index of image in original array used
+  insertZeros(perViewErrorsVector);
 
   //Getting the index of the element with the most error, adding it to vector of image-indices to ignore
   int maxElementIndex = std::max_element(perViewErrorsVector.begin(), perViewErrorsVector.end()) - perViewErrorsVector.begin();
   imagesToIgnore.push_back(maxElementIndex);
+  
+  //getIndex(images,perViewErrorsVector[maxElementIndex])
+  //int imageToIgnoreIndex = getIndex(images,perViewErrorsVector[maxElementIndex]);
+  //imagesToIgnore.push_back(imageToIgnoreIndex);
 
   if(noImagesUsed - imagesToIgnore.size() == minImages) {
     maxIterationsReached = true;
