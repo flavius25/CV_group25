f736dc39a1257bc1f91471cc9fee1cdd79219591
dollbo <annadolly.dollbo@gmail.com>
2022-02-25 11:52:40 +0100

    Files for background blending

---
+++ data/BackgroundBlending.cpp
@@ -0,0 +1,111 @@
+
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include <iostream>
+
+using namespace cv;
+
+// using std::cin;
+// using std::cout;
+// using std::endl;
+
+int main( void )
+{
+  //Declaration of vector with images
+  std::vector<cv::String> images;
+
+  // Path of the folder containing checkerboard images
+  std::string path = "./cam1/background_images/*.png";
+
+  cv::glob(path, images);
+
+  std::vector<cv::Mat> image_matrices;
+  for (int i{0}; i < images.size(); i++){
+    cv::Mat this_image = imread(images[i]);
+    image_matrices.push_back(this_image);
+  }
+
+  cv::Mat dst;
+  cv::Mat mean_img = image_matrices[0];
+  for (int i{0}; i < image_matrices.size(); i++){
+    if (i == 0){
+      continue;
+    }
+    else {
+      double alpha = 0.5;
+      double beta = (1.0 - alpha);
+      addWeighted(image_matrices[i], alpha, mean_img, beta, 0.0, dst);
+    }
+  }
+
+  imwrite("test.png", mean_img);
+  imread("test.png");
+  imshow("This window", dst);
+  waitKey(0);
+  return 0;
+
+}
+
+
+// for i in range(len(image_data)):
+//     if i == 0:
+//         pass
+//     else:
+//         alpha = 1.0/(i + 1)
+//         beta = 1.0 - alpha
+//         avg_image = cv2.addWeighted(image_data[i], alpha, avg_image, beta, 0.0)
+
+// cv2.imwrite('avg_happy_face.png', avg_image)
+// avg_image = cv2.imread('avg_happy_face.png')
+// plt.imshow(avg_image)
+// plt.show()
+
+
+
+
+
+
+
+  //  double alpha = 0.5; double beta; double input;
+
+  //  Mat src1, src2, dst;
+  //  std::cout << " Simple Linear Blender " << std::endl;
+  //  std::cout << "-----------------------" << std::endl;
+  //  std::cout << "* Enter alpha [0.0-1.0]: ";
+  //  std::cin >> input;
+  //  // We use the alpha provided by the user if it is between 0 and 1
+  //  if( input >= 0 && input <= 1 )
+  //    { alpha = input; }
+  //  src1 = imread( samples::findFile("LinuxLogo.jpg") );
+  //  src2 = imread( samples::findFile("WindowsLogo.jpg") );
+  //  if( src1.empty() ) { std::cout << "Error loading src1" << std::endl; return EXIT_FAILURE; }
+  //  if( src2.empty() ) { std::cout << "Error loading src2" << std::endl; return EXIT_FAILURE; }
+  //  beta = ( 1.0 - alpha );
+  //  addWeighted( src1, alpha, src2, beta, 0.0, dst);
+  //  imshow( "Linear Blend", dst );
+  //  waitKey(0);
+  //  return 0;
+
+
+// # import all image files with the .jpg extension
+// images = glob.glob ("happy_faces/*.jpg")
+
+
+// image_data = []
+// for img in images:
+//     this_image = cv2.imread(img, 1)
+//     image_data.append(this_image)
+
+// avg_image = image_data[0]
+// for i in range(len(image_data)):
+//     if i == 0:
+//         pass
+//     else:
+//         alpha = 1.0/(i + 1)
+//         beta = 1.0 - alpha
+//         avg_image = cv2.addWeighted(image_data[i], alpha, avg_image, beta, 0.0)
+
+// cv2.imwrite('avg_happy_face.png', avg_image)
+// avg_image = cv2.imread('avg_happy_face.png')
+// plt.imshow(avg_image)
+// plt.show()
