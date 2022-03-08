f3c37eb0f7c538ae6c81c7685a44449c08b34db1
dollbo <annadolly.dollbo@gmail.com>
2022-03-08 14:49:07 +0100

    Added label per voxel, added Clustering file

--- VoxelReconstruction/src/controllers/Reconstructor.h
+++ VoxelReconstruction/src/controllers/Reconstructor.h
@@ -30,6 +30,7 @@ public:
 		cv::Vec3f color;                   // Color vector double
 		std::vector<cv::Point> camera_projection;  // Projection location for camera[c]'s FoV (2D)
 		std::vector<int> valid_camera_projection;  // Flag if camera projection is in camera[c]'s FoV
+		int label; 									//label which person it is
 	};
 
 private:
