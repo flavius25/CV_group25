d46c48ba7a5dd53f6f85400275c4b505be309de0
dollbo <annadolly.dollbo@gmail.com>
2022-03-22 11:50:28 +0100

    Plotting some training images with labels

--- Assig4/Assig4.py
+++ Assig4/Assig4.py
@@ -3,10 +3,29 @@ from keras.datasets import fashion_mnist
 import matplotlib.pyplot as plt
 
 
+# Loading the data, splitting up into test and trainign set 
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
-print(len(train_images), len(train_labels), len(test_images))
+#print(len(train_images), len(test_images))   #checking original sizes 
+
+# Use 10,000 samples for validation, the rest (50,000) is for training set
+validation_images = train_images[-10000:]
+train_images = train_images[:-10000]
+
+#Setting class names so we can get an overview of the data
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
 
+#Plotting a subset of data with labels
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i])
+    plt.xlabel(class_names[train_labels[i]])
+plt.show()
 
 
 
