4860c08128c74213f1568688082891da95d1f835
dollbo <annadolly.dollbo@gmail.com>
2022-03-22 11:25:49 +0100

    Loaded dataset, checked size

--- Assig4/Assig4.py
+++ Assig4/Assig4.py
@@ -1,2 +1,13 @@
 import tensorflow as tf
 from keras.datasets import fashion_mnist
+import matplotlib.pyplot as plt
+
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+print(len(train_images), len(train_labels), len(test_images))
+
+
+
+
+
+
