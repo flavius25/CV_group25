b7d7e380e70008fdab7f8b127b90893e2cb2b0ef
dollbo <annadolly.dollbo@gmail.com>
2022-04-05 16:41:06 +0200

    Error in one hot encoding, I'll fix tomorrow

--- CNN_model.py
+++ CNN_model.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-from sklearn.model_selection import train_test_split
 from keras import layers
 from keras.callbacks import TensorBoard, LearningRateScheduler
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
@@ -7,9 +6,8 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import figure
 import efficientnet.keras as efn 
-from keras.models import Sequential
-from keras import layers
 from utils import *
+from keras.models import Sequential
 
 """ Global variables """
 IMG_SIZE = (224,224)
@@ -17,45 +15,20 @@ EPOCHS = 50
 BATCH_SIZE = 64
 NUM_CLASSES = 40 # 40 classes in the Stanford40 dataset
 
-needDataDirs = True # set to False if images already sorted and available
-
-createDataDirectories(needDataDirs)
 
 """ Load dataset """
-#Load the dataset which has already been preprocessed
-SF_training_set,  SF_test_set = loadSF40(img_size =  IMG_SIZE)
+#Load the dataset which has already been preprocessed, set needDirectories to False if SF_train, SF_test, and SF_validation dirs already exist
+train_ds, test_ds, val_ds, train_labels, test_labels, validation_labels, class_names = loadSF40(img_size =  IMG_SIZE, needDirectories=False)
 
-train_labels = []
-train_images = []
-
-#Unbatch and extract images and labels from training dataset to do validation split with stratification
-for images, labels in SF_training_set.unbatch():
-    train_labels.append(labels)
-    train_images.append(images)
-
-
-#Split the trainingset to obtain 10% stratified validation set
-train_images, validation_images, train_labels, validation_labels = train_test_split(train_images, train_labels, test_size=0.1, random_state=0, stratify=train_labels)
-
-#convert back to tf Dataset object
-train_images = tf.data.Dataset.from_tensor_slices(train_images)
 
 #no of train and validation images
 NO_TRAIN_IMGS = len(train_labels)
 NO_VAL_IMGS = len(validation_labels)
 
-#Get iterator for data augmentation and 
-train_iterator = getIterator(train_images, train_labels)
-validation_iterator =  getIterator(validation_images, validation_labels)
-test_iterator = getIterator(SF_test_set, test_labels)
 
-""" This is how we will fit it with the model """
-# fit model with generator
-# model.fit_generator(train_iterator, steps_per_epoch=len(train_iterator), epochs=5)
-# # evaluate model
-# _, acc = model.evaluate_generator(test_iterator, steps=len(test_iterator), verbose=0)
+#https://www.tensorflow.org/api_docs/python/tf/data/Dataset#from_tensor_slices
+#http://man.hubwiz.com/docset/TensorFlow_2.docset/Contents/Resources/Documents/tf/data/Dataset.html 
 
-# https://machinelearningmastery.com/how-to-normalize-center-and-standardize-images-with-the-imagedatagenerator-in-keras/ 
 
 """ One-hot encoding """
 
@@ -63,28 +36,30 @@ def onehot_encoding(image, label):
     label = tf.one_hot(label, NUM_CLASSES)
     return image, label
 
+#Apply one hot encoding to the different datasets
+train_ds = train_ds.map(onehot_encoding, num_parallel_calls=tf.data.AUTOTUNE)
+train_ds = train_ds.batch(batch_size=BATCH_SIZE, drop_remainder=True)
+train_ds = train_ds.prefetch(tf.data.AUTOTUNE)
 
-train_images = train_images.map(
-    onehot_encoding, num_parallel_calls=tf.data.AUTOTUNE)
+val_ds = val_ds.map(onehot_encoding, num_parallel_calls=tf.data.AUTOTUNE)
+val_ds = val_ds.batch(batch_size=BATCH_SIZE, drop_remainder=True)
+val_ds = val_ds.prefetch(tf.data.AUTOTUNE)
 
-train_images = train_images.batch(batch_size=BATCH_SIZE, drop_remainder=True)
-train_images = train_images.prefetch(tf.data.AUTOTUNE)
+test_ds = test_ds.map(onehot_encoding)
+test_ds = test_ds.batch(batch_size=BATCH_SIZE, drop_remainder=True)
 
-validation_images = validation_images.map(
-    onehot_encoding, num_parallel_calls=tf.data.AUTOTUNE)
-
-validation_images = validation_images.batch(batch_size=BATCH_SIZE, drop_remainder=True)
-validation_images = validation_images.prefetch(tf.data.AUTOTUNE)
-
-SF_test_set = SF_test_set.map(onehot_encoding)
-SF_test_set = SF_test_set.batch(batch_size=BATCH_SIZE, drop_remainder=True)
+print("Done one-hot encoding!")
 
 """ Build & Train Stanford40 Model EfficientNet"""
 
 # Define the input and output layers of the model 
 inputs = layers.Input(shape=(IMG_SIZE[0], IMG_SIZE[1], 3))
 
-outputs = efn.EfficientNetB0(include_top=True, weights=None, classes=class_names) #Include top set to true as we are not using pre-trained weights but training from scratch
+#Fetch image augmentation layers and apply to inputs
+img_augmentation = dataAugmentation()
+x = img_augmentation(inputs)
+
+outputs = efn.EfficientNetB0(include_top=True, weights=None, classes=class_names)(x) #Include top set to true as we are not using pre-trained weights but training from scratch
 
 # Initialise the model. Compule and show summary. 
 model = tf.keras.Model(inputs, outputs)
@@ -93,13 +68,17 @@ model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accur
 model.summary()
 
 #Fit the model with the data augmentation and normalisation iterators
-history = model.fit_generator(train_iterator, epochs = EPOCHS, steps_per_epoch = NO_TRAIN_IMGS//BATCH_SIZE, 
-                              validation_data = validation_iterator, validation_steps=NO_VAL_IMGS//BATCH_SIZE, 
+history = model.fit(train_ds, epochs = EPOCHS, steps_per_epoch = NO_TRAIN_IMGS//BATCH_SIZE, 
+                              validation_data = val_ds, validation_steps=NO_VAL_IMGS//BATCH_SIZE, 
                               verbose=1, use_multiprocessing=True, workers=4)
 
 #Save weights as we need these for transfer learning 
 model.save_weights("model_weights.h5")
 
+
+
+#plotAccuracy("title", train_acc, val_acc)                     # made these plotting functions in utils for hassle-free plotting
+#plotLoss("title", train_loss, val_loss)
 
 """ Building transfer learning model """
 
