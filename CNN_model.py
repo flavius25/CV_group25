df48345f10ee2b54071d4efc623a917ded7492fa
dollbo <annadolly.dollbo@gmail.com>
2022-04-13 12:07:58 +0200

    Added loading for TVHIdataset

--- CNN_model.py
+++ CNN_model.py
@@ -11,12 +11,12 @@ from keras.models import Sequential
 
 """ Global variables """
 IMG_SIZE = (224,224)
-EPOCHS = 50
+EPOCHS = 12
 BATCH_SIZE = 64
 NUM_CLASSES = 40 # 40 classes in the Stanford40 dataset
 
 
-""" Load dataset """
+""" Load Stanford40 dataset """
 #Load the dataset which has already been preprocessed, set needDirectories to False if SF_train, SF_test, and SF_validation dirs already exist
 train_ds, test_ds, val_ds, train_labels, test_labels, validation_labels, class_names = loadSF40(img_size =  IMG_SIZE, needDirectories=False)
 
@@ -38,15 +38,15 @@ def onehot_encoding(image, label):
 
 #Apply one hot encoding to the different datasets
 train_ds = train_ds.map(onehot_encoding, num_parallel_calls=tf.data.AUTOTUNE)
-train_ds = train_ds.batch(batch_size=BATCH_SIZE, drop_remainder=True)
+#train_ds = train_ds.batch(batch_size=BATCH_SIZE, drop_remainder=True)
 train_ds = train_ds.prefetch(tf.data.AUTOTUNE)
 
 val_ds = val_ds.map(onehot_encoding, num_parallel_calls=tf.data.AUTOTUNE)
-val_ds = val_ds.batch(batch_size=BATCH_SIZE, drop_remainder=True)
+#val_ds = val_ds.batch(batch_size=BATCH_SIZE, drop_remainder=True)
 val_ds = val_ds.prefetch(tf.data.AUTOTUNE)
 
 test_ds = test_ds.map(onehot_encoding)
-test_ds = test_ds.batch(batch_size=BATCH_SIZE, drop_remainder=True)
+#test_ds = test_ds.batch(batch_size=BATCH_SIZE, drop_remainder=True)
 
 print("Done one-hot encoding!")
 
@@ -59,7 +59,7 @@ inputs = layers.Input(shape=(IMG_SIZE[0], IMG_SIZE[1], 3))
 img_augmentation = dataAugmentation()
 x = img_augmentation(inputs)
 
-outputs = efn.EfficientNetB0(include_top=True, weights=None, classes=class_names)(x) #Include top set to true as we are not using pre-trained weights but training from scratch
+outputs = efn.EfficientNetB0(include_top=True, weights=None, classes=NUM_CLASSES)(x) #Include top set to true as we are not using pre-trained weights but training from scratch
 
 # Initialise the model. Compule and show summary. 
 model = tf.keras.Model(inputs, outputs)
@@ -67,20 +67,23 @@ model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accur
 
 model.summary()
 
-#Fit the model with the data augmentation and normalisation iterators
-history = model.fit(train_ds, epochs = EPOCHS, steps_per_epoch = NO_TRAIN_IMGS//BATCH_SIZE, 
-                              validation_data = val_ds, validation_steps=NO_VAL_IMGS//BATCH_SIZE, 
-                              verbose=1, use_multiprocessing=True, workers=4)
+#Fit the model with the data augmentation and normalisation iterators    ### , steps_per_epoch = NO_TRAIN_IMGS//BATCH_SIZE ## validation_steps=NO_VAL_IMGS//BATCH_SIZE, 
+history = model.fit(train_ds, epochs = EPOCHS, validation_data = val_ds, verbose=2, use_multiprocessing=True, workers=4)
 
 #Save weights as we need these for transfer learning 
 model.save_weights("model_weights.h5")
 
+SFweights_path = "model_weights.h5"
 
-
-#plotAccuracy("title", train_acc, val_acc)                     # made these plotting functions in utils for hassle-free plotting
-#plotLoss("title", train_loss, val_loss)
+plotAccuracy("title", train_acc, val_acc)                     # made these plotting functions in utils for hassle-free plotting
+plotLoss("title", train_loss, val_loss)
 
 """ Building transfer learning model """
+NUM_CLASSES = 4
+
+""" Load dataset """
+#Load the dataset which has already been preprocessed, set needDirectories to False if SF_train, SF_test, and SF_validation dirs already exist
+train_ds, test_ds, val_ds, train_labels, test_labels, validation_labels, class_names = loadTVHI(img_size =  IMG_SIZE, needDirectories=False)
 
 TL_model = 4
 
